534ce5e643d994b186342bcb5d20040e3200e569
Spencer Blum <spencerblum@gmail.com>
2020-01-30 18:28:22 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,11 +4,11 @@ require 'pp'
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
-pp "nds"
+
 
 def gross_for_director(director_data)
 
-pp "director_data"
+pp 'director_data'
 
 
 end
